338714c339fe5a57ffea243450c711028662448e
deepkashyap07 <deepanshukumar2580@gmail.com>
2025-04-19 17:24:59 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-store-frontend-teal-six.vercel.app/'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://book-store-frontend-teal-six.vercel.app",
+      "https://book-store-frontend-izwbo1eji.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 // routes
-const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const bookRoutes = require("./src/books/book.route");
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
@@ -31,7 +37,9 @@ async function main() {
   });
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main()
+  .then(() => console.log("Mongodb connect successfully!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
